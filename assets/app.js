0869dacd2b49f8f798f04884e8f48f8a48ef2716
Alexandra Shields <alexandrashields@Alexandras-MacBook-Pro.local>
2019-08-08 18:49:42 -0400

    js

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBm6pRi90hU-NU0aiDJzpYI1FRvtUDgoKs",
+    authDomain: "test-682b3.firebaseapp.com",
+    databaseURL: "https://test-682b3.firebaseio.com",
+    projectId: "test-682b3",
+    storageBucket: "test-682b3.appspot.com",
+    messagingSenderId: "804469624379"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
